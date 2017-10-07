65838eb665c4a8550a086b2bbbb01786c16d715b
FletcherTime <32583559+FletcherTime@users.noreply.github.com>
2017-10-07 10:59:43 -0400

    Basic main menu
    Added main menu with functional buttons

---
+++ Assets/Resources/Scripts/MainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+
+    public void ExitButton()
+    {
+        Debug.Break();
+        Application.Quit();
+    }
+}
