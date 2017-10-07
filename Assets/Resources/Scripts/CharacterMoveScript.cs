54d263716ba936c52892855eeb555f5f3f82fa53
barrettand <barrett.and@husky.neu.edu>
2017-10-07 00:13:22 -0400

    Basic Combat Mechanics
    We can butt players together, jump and move them.

--- Assets/Resources/Scripts/CharacterMoveScript.cs
+++ Assets/Resources/Scripts/CharacterMoveScript.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMoveScript : MonoBehaviour {
-    public int dir;
-    public KeyCode keyLeft, keyRight, keyThrust;
+    public GameObject opponent;
+    public int dir, buttpower;
+    public KeyCode keyLeft, keyRight, keyThrust, keySuperThrust;
+    public bool onGround;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,17 +14,42 @@ public class CharacterMoveScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (opponent.transform.position.x > transform.position.x) {
+            dir = 1;
+        }
+        if (opponent.transform.position.x < transform.position.x)
+        {
+            dir = -1;
+        }
+        transform.localScale = new Vector3(dir, transform.localScale.y, transform.localScale.z);
         if (Input.GetKey(keyLeft))
         {
-            transform.Translate(-0.05f, 0, 0);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0));
         }
         if (Input.GetKey(keyRight))
         {
-            transform.Translate(0.05f, 0, 0);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 0));
         }
-        if (Input.GetKeyDown(keyThrust))
+        if (Input.GetKeyDown(keyThrust) && onGround)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(500 * dir, 100));
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(buttpower * dir, 200));
+            onGround = false;
+        }
+        if (Input.GetKeyDown(keySuperThrust))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 500));
+            onGround = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.name == "Arena") {
+            onGround = true;
+        }
+        if (col.gameObject.GetComponent<Rigidbody2D>())
+        {
+            col.gameObject.GetComponent<Rigidbody2D>().velocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-col.gameObject.GetComponent<CharacterMoveScript>().buttpower * 0.5f * dir, 0));
         }
     }
 }
