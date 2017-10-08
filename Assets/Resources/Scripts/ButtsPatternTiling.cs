8cd52ec16181f99da04646e8639fcec8c3273b98
barrettand <barrett.and@husky.neu.edu>
2017-10-07 20:25:46 -0400

    NEW ART
    BOOSH!

---
+++ Assets/Resources/Scripts/ButtsPatternTiling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtsPatternTiling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<RectTransform>().anchoredPosition.x >= 800)
+        {
+            GetComponent<RectTransform>().anchoredPosition = new Vector2(-800, 0);
+        }
+        GetComponent<RectTransform>().Translate(1f, 0, 0);
+	}
+}
